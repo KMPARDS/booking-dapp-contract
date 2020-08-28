08c413eb9f4c16a8a40f8aa05b11f294caeb1bf9
Milli Guleria <miliguleria15@gmail.com>
2020-08-28 12:01:58 +0530

    commit

--- contracts/bookingdapp.sol
+++ contracts/bookingdapp.sol
@@ -9,13 +9,21 @@ contract OpenEvents {
 	address tokenAddress;
 
 	struct OpenEvent {
+		
+		//event details
 		address owner;
 		string name;
 		uint time;
-		uint price;
+		
+		//different types of tickets
+		uint price_front;
+		uint price_centre;
+		uint price_back;
+		
 		bool limited;
 		uint seats;
 		uint sold;
+		uint wallet;
 	}
 	
 	OpenEvent[] private openEvents;
@@ -28,49 +36,70 @@ contract OpenEvents {
 	Ticket[] private tickets;
 
     event CreatedEvent(address indexed owner, uint eventId);
+    
     event SoldTicket(address indexed buyer, uint indexed eventId);
 	
-	/*constructor(address _token) public {
-		tokenAddress = _token;
-	} */
+	/// KYC DApp
+    /// @notice if your platform needs to store user information like name, you can instead use KycDapp 
+    /// @dev https://github.com/KMPARDS/esn-contracts/blob/master/contracts/ESN/KycDapp/IKycDapp.sol
+    interface KycDapp
+    {
+        function isKycLevel1(address _wallet) external view returns (bool);
 
-    modifier goodTime(uint _time) {
+        function isKycLevel2(address _wallet, address _platform) external view returns (bool);
+    }
+    
+    modifier onlyKycApproved()
+    {
+        require(kycDappContract.isKycLevel1(msg.sender), 'KYC is not approved');
+        _;
+    }
+
+    modifier goodTime(uint _time)
+    {
         require(_time > now);
         _;
     }
 
-    modifier eventExist(uint _id) {
+    modifier eventExist(uint _id)
+    {
         require(_id < openEvents.length);
         _;
     }
 
-	function createEvent(string memory _name, uint _time, uint _price, bool _limited, uint _seats) goodTime(_time) public
+	function createEvent(string memory _name, uint _time, 	uint _price_front, uint _price_centre, uint _price_back, bool _limited, uint _seats) goodTime(_time) public
 	{
 		OpenEvent memory _event = OpenEvent({
 			owner: msg.sender,
 			name: _name,
 			time: _time,
 			limited: _limited,
-			price: _price,
+	        price_front: _price_front,
+		    price_centre: _price_centre,
+		    price_back: _price_back,
 			seats: _seats,
-			sold: 0
+			sold: 0,
+			wallet: 0
 		});
 		openEvents.push(_event);
 		uint _eventid= openEvents.length;
 		emit CreatedEvent(msg.sender, _eventid);
 	}
 
-	function getEvent(uint _id) public view eventExist(_id) returns(string memory name,	uint time, uint price, bool limited, uint seats, uint sold, address owner)
+	function getEvent(uint _id) public view eventExist(_id) returns(string memory name,	uint time,	uint price_front,uint price_centre, uint price_back, bool limited, uint seats, uint sold, address owner, uint wallet)
 		{
 		    OpenEvent memory _event = openEvents[_id];
 		    return(
 			    _event.name,
 			    _event.time,
-			    _event.price,
+			    _event.price_front,
+			    _event.price_centre,
+			    _event.price_back,
 			    _event.limited,
 			    _event.seats,
 			    _event.sold,
-			    _event.owner
+			    _event.owner,
+			    _event.wallet
 		    );
 	    }
 
@@ -80,18 +109,20 @@ contract OpenEvents {
 	
 	// Function to buy ticket to specific event.
 	
-	function buyTicket(uint _eventId) public payable eventExist(_eventId) goodTime(openEvents[_eventId].time)
+	function buyTicket(uint _eventId, uint seatno) public payable eventExist(_eventId) goodTime(openEvents[_eventId].time)
 	{
-		require(openEvents[_eventId].price == msg.value);
+		require(openEvents[_eventId].price_front == msg.value, "Front seat booked");
+		require(openEvents[_eventId].price_centre == msg.value, "Centre seat booked");
+		require(openEvents[_eventId].price_back == msg.value, "Back seat booked");
 		
-		OpenEvent memory _event = openEvents[_eventId];
-
-	    uint _seat = _event.sold + 1 ;
+		openEvents[_eventId].wallet += msg.value;
+	
+	    uint _seat = openEvents[_eventId].sold + 1 ;
 	    openEvents[_eventId].sold = _seat;
 		
 		Ticket memory _ticket = Ticket({
 			event_id: _eventId,
-			seat: _seat
+			seat: seatno
 		});
 
 		emit SoldTicket(msg.sender, _eventId);
@@ -104,15 +135,41 @@ contract OpenEvents {
 		return(_ticket.event_id, _ticket.seat);
 	}
 	
-	function cancelTicket(uint _eventid, uint _seatno) public
+	function cancelTicket(uint _eventid, uint _seatno, uint _time) public payable
 	{
-
+	    
+	    //Ticket cannot be cancelled once the event has started 
+	    require(_time>now, "Cannot cancel the ticket, event has started!");
+	    
+	    uint wallet_ = openEvents[_eventid].wallet;
+	   
+	    //80% of the ticket price refunded back when ticket is cancelled within 48 hours before the event
+	    //100% ticket price refunded if ticket cancelled before 48 hours of the event 
+	    
+	    if( openEvents[_eventid].time - now <48 && msg.value== openEvents[_eventid].price_front)
+	        openEvents[_eventid].wallet = wallet_ - ((openEvents[_eventid].price_front*80)/100);
+	   
+	    else if(openEvents[_eventid].time - now > 48  && msg.value== openEvents[_eventid].price_front) 
+	        openEvents[_eventid].wallet = wallet_ - (openEvents[_eventid].price_front);
+	    
+	    if(openEvents[_eventid].time - now < 48 && msg.value== openEvents[_eventid].price_centre)
+	        openEvents[_eventid].wallet = wallet_ - ((openEvents[_eventid].price_centre*80)/100);
+	   
+	    else if(openEvents[_eventid].time - now > 48 && msg.value== openEvents[_eventid].price_centre)
+	        openEvents[_eventid].wallet = wallet_ - (openEvents[_eventid].price_centre);
+	   
+	    if(openEvents[_eventid].time - now < 48 && msg.value== openEvents[_eventid].price_back)
+	        openEvents[_eventid].wallet = wallet_ - ((openEvents[_eventid].price_back*80)/100);
+	   
+	    else if(openEvents[_eventid].time - now > 48 && msg.value== openEvents[_eventid].price_centre)
+	        openEvents[_eventid].wallet = wallet_ - (openEvents[_eventid].price_back);
+       
         openEvents[_eventid].sold -= 1;
         delete tickets[_seatno];
 
 	}
 	
-	function cancelevent(uint _eventid) public 
+	function cancelevent(uint _eventid) public goodTime(openEvents[_eventid].time)
 	{
 	   delete openEvents[_eventid];
 	}
