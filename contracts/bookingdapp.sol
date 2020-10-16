587ea2ae3f0bc1da0ec1beba80f987d4d59628f5
Mridul Dey <mridul.dgp2015@gmail.com>
2020-10-16 09:44:15 +0530

    modify bookingdapp contract

--- contracts/bookingdapp.sol
+++ contracts/bookingdapp.sol
@@ -1,102 +1,137 @@
-pragma solidity ^0.6.11;
+// SPDX-License-Identifier: MIT
 
-import './KycDapp.sol';
-import './daySwappers.sol';
+pragma solidity ^0.7.0;
 
-/**
-* Title OpenEvents
-* It is a main contract that provides ability to create events and view information about events. 
-*It is a main contract that provides ability to buy tickets and view tickets.
-*It is a main contract that provides ability cancel tickets and events.
-*/
+import './SafeMath.sol';
 
-contract OpenEvents {
+
+contract BookingDapp 
+{
+    using SafeMath for uint256;
+    
 	address tokenAddress;
 
-	struct OpenEvent {
+	struct OpenEvent 
+	{
+// 		event details
+		address event_owner;
+		string event_name;
+		uint256 event_id;
+		uint256 start_time;
+		string event_location;
+		uint256 seat_types;
 		
-		//event details
-		address owner;
-		string name;
-		uint time;
+// 		price of different types of tickets
+		uint256[] prices;
 		
-		//different types of tickets
-		uint price_front;
-		uint price_centre;
-		uint price_back;
+// 		bool limited;
 		
-		bool limited;
-		uint seats;
-		uint sold;
-		uint wallet;
+// 		number of seats of different types
+		uint256[] seat_numbers;
+		
+// 		total number of seats
+        uint256 total_seats;
+		
+// 		event collection wallet
+		uint256 wallet;
+		
+// 		visited array for seats booked
+        uint256[2][] booked;
+        
+//      For having record of seats and person booking them
+        // mapping(uint256 => address) record;
+        
 	}
+	
+	mapping(uint256 => mapping(uint256 => address)) Record;
 	
 	OpenEvent[] private openEvents;
 	
-	struct Ticket {
-		uint event_id;
-		uint seat;
-	}
+	uint256[] eventExists;
+// 	struct Ticket 
+// 	 {
+// 		uint event_id;
+// 		uint256 seat;
+// 	}
 	
-	Ticket[] private tickets;
+// 	Ticket[] private tickets;
 
-    event CreatedEvent(address indexed owner, uint eventId);
+    event CreatedEvent(address indexed owner, uint256 eventId);
     
-    event SoldTicket(address indexed buyer, uint indexed eventId);
+    event SoldTicket(address indexed buyer, uint256 indexed eventId, uint256[] seats);
 	
-    modifier onlyKycApproved()
-    {
-        require(kycDappContract.isKycLevel1(msg.sender), 'KYC is not approved');
-        _;
-    }
+    // modifier onlyKycApproved()
+    // {
+    //     require(kycDappContract.isKycLevel1(msg.sender), 'KYC is not approved');
+    //     _;
+    // }
 
     modifier goodTime(uint _time)
     {
-        require(_time > now);
+        require(_time > block.timestamp);
         _;
     }
 
     modifier eventExist(uint _id)
     {
-        require(_id < openEvents.length);
+        require(eventExists[_id] == 1);
         _;
     }
 
-	function createEvent(string memory _name, uint _time, 	uint _price_front, uint _price_centre, uint _price_back, bool _limited, uint _seats) goodTime(_time) public
+	function createEvent(string memory _name, uint _time, string memory _location, uint256 _types, uint256[] memory _prices, uint256[] memory _seats) goodTime(_time) public
 	{
+        uint256[2][] memory _booked;
+		uint256[] memory a;
+		for(uint256 i=1;i<_types;i++)
+		    a[i] += a[i-1];
+		
+		uint256 seat=1;
+		for(uint256 i=0;i<_types;i++)
+		{
+		    while(seat<=a[i])
+		    {
+		        _booked[seat][0] = 0;
+		        _booked[seat][1] = i+1;
+		        seat++;
+		    }
+		}
+		
 		OpenEvent memory _event = OpenEvent({
-			owner: msg.sender,
-			name: _name,
-			time: _time,
-			limited: _limited,
-	        price_front: _price_front,
-		    price_centre: _price_centre,
-		    price_back: _price_back,
-			seats: _seats,
-			sold: 0,
-			wallet: 0
+			event_owner: msg.sender,
+			event_name: _name,
+			event_id: openEvents.length,
+			start_time: _time,
+			event_location: _location,
+// 			limited: _limited,
+            seat_types: _types,
+            prices: _prices,
+            seat_numbers: _seats,
+            total_seats: a[_types-1],
+            wallet: 0,
+            booked: _booked
 		});
+		
 		openEvents.push(_event);
-		uint _eventid= openEvents.length;
-		emit CreatedEvent(msg.sender, _eventid);
+		eventExists[openEvents.length-1] = 1;
+		emit CreatedEvent(msg.sender, openEvents.length-1);
 	}
 
-	function getEvent(uint _id) public view eventExist(_id) returns(string memory name,	uint time,	uint price_front,uint price_centre, uint price_back, bool limited, uint seats, uint sold, address owner, uint wallet)
-		{
-		    OpenEvent memory _event = openEvents[_id];
-		    return(
-			    _event.name,
-			    _event.time,
-			    _event.price_front,
-			    _event.price_centre,
-			    _event.price_back,
-			    _event.limited,
-			    _event.seats,
-			    _event.sold,
-			    _event.owner,
-			    _event.wallet
-		    );
-	    }
+	function getEvent(uint _id) public view eventExist(_id) returns(address, string memory, uint256, uint256, string memory, uint256, uint256[] memory, uint256[] memory, uint256, uint256[2][] memory)
+	{
+	    OpenEvent memory _event = openEvents[_id];
+	    return(
+		    _event.event_owner,
+		    _event.event_name,
+		    _event.event_id,
+		    _event.start_time,
+		    _event.event_location,
+		    _event.seat_types,
+		    _event.prices,
+		    _event.seat_numbers,
+		    _event.wallet,
+		    _event.booked
+	    );
+	}
 
 	function getEventsCount() public view returns(uint) {
 		return openEvents.length;
@@ -104,68 +139,98 @@ contract OpenEvents {
 	
 	// Function to buy ticket to specific event.
 	
-	function buyTicket(uint _eventId, uint seatno) public payable eventExist(_eventId) goodTime(openEvents[_eventId].time)
+	function buyTicket(uint _eventId, uint[] memory seatno, uint256 amt) public payable eventExist(_eventId) /*goodTime(openEvents[_eventId].start_time)*/
 	{
-		require(openEvents[_eventId].price_front == msg.value, "Front seat booked");
-		require(openEvents[_eventId].price_centre == msg.value, "Centre seat booked");
-		require(openEvents[_eventId].price_back == msg.value, "Back seat booked");
+	    OpenEvent memory _event = openEvents[_eventId];
+	    
+	    uint check=0;
+		for(uint256 i=0;i<seatno.length;i++)
+		{
+		    if(_event.seat_numbers[_event.booked[seatno[i]][1] - 1]>0 || _event.booked[seatno[i]][0] == 1)
+		    {
+		        check=1;
+		        break;
+		    }
+		}
 		
-		openEvents[_eventId].wallet += msg.value;
-	
-	    uint _seat = openEvents[_eventId].sold + 1 ;
-	    openEvents[_eventId].sold = _seat;
+		require(check==0, "Seats selected have been booked");
 		
-		Ticket memory _ticket = Ticket({
-			event_id: _eventId,
-			seat: seatno
-		});
+		require(msg.value == amt, "Require price of tickets");
+		
+		for(uint256 i=0;i<seatno.length;i++)
+		{
+		    _event.booked[seatno[i]][0]=1;
+		    _event.seat_numbers[_event.booked[seatno[i]][1] - 1] -= 1;
+		  //  _event.record[seatno[i]] = msg.sender;
+		    Record[_eventId][seatno[i]] = msg.sender;
+		}
+		
+		_event.wallet += amt;
+		
 
-		emit SoldTicket(msg.sender, _eventId);
+		emit SoldTicket(msg.sender, _eventId, seatno);
 	}
 	
-	function getTicket(uint _id) public view returns(uint, uint) 
-	{
-		require(_id < tickets.length);
-		Ticket memory _ticket = tickets[_id];
-		return(_ticket.event_id, _ticket.seat);
-	}
+// 	function getTicket(uint _id) public view returns(uint, uint) 
+// 	{
+// 		require(_id < tickets.length);
+// 		Ticket memory _ticket = tickets[_id];
+// 		return(_ticket.event_id, _ticket.seat);
+// 	}
 	
-	function cancelTicket(uint _eventid, uint _seatno, uint _time) public payable
+	function cancelTicket(uint _eventId, uint[] memory seatno, uint _time) eventExist(_eventId) public payable
 	{
+	    
+	    OpenEvent memory _event = openEvents[_eventId];
 	    
 	    //Ticket cannot be cancelled once the event has started 
-	    require(_time>now, "Cannot cancel the ticket, event has started!");
+	    require(_time>=_event.start_time, "Cannot cancel the ticket, event has started!");
 	    
-	    uint wallet_ = openEvents[_eventid].wallet;
-	   
+
 	    //80% of the ticket price refunded back when ticket is cancelled within 48 hours before the event
 	    //100% ticket price refunded if ticket cancelled before 48 hours of the event 
 	    
-	    if( openEvents[_eventid].time - now <48 && msg.value== openEvents[_eventid].price_front)
-	        openEvents[_eventid].wallet = wallet_ - ((openEvents[_eventid].price_front*80)/100);
+	    if( _event.start_time - block.timestamp <= 48*3600)
+	    {
+	        for(uint256 i=0;i<seatno.length;i++)
+	        {
+    	        _event.wallet -= (_event.prices[_event.booked[seatno[i]][1] - 1]).mul(80).div(100);
+    	        _event.booked[seatno[i]][0]=0;
+    		    _event.seat_numbers[_event.booked[seatno[i]][1] - 1] += 1;
+    		  //  _event.record[seatno[i]] = address(0);
+    		    Record[_eventId][seatno[i]] = address(0);
+	        }
+	    }
+	    else
+	    {
+	        for(uint256 i=0;i<seatno.length;i++)
+	        {
+    	        _event.wallet -= _event.prices[_event.booked[seatno[i]][1] - 1];
+    	        _event.booked[seatno[i]][0]=0;
+    		    _event.seat_numbers[_event.booked[seatno[i]][1] - 1] += 1;
+    		  //  _event.record[seatno[i]] = address(0);
+    		    Record[_eventId][seatno[i]] = address(0);
+
+	        }
+	    }
 	   
-	    else if(openEvents[_eventid].time - now > 48  && msg.value== openEvents[_eventid].price_front) 
-	        openEvents[_eventid].wallet = wallet_ - (openEvents[_eventid].price_front);
-	    
-	    if(openEvents[_eventid].time - now < 48 && msg.value== openEvents[_eventid].price_centre)
-	        openEvents[_eventid].wallet = wallet_ - ((openEvents[_eventid].price_centre*80)/100);
-	   
-	    else if(openEvents[_eventid].time - now > 48 && msg.value== openEvents[_eventid].price_centre)
-	        openEvents[_eventid].wallet = wallet_ - (openEvents[_eventid].price_centre);
-	   
-	    if(openEvents[_eventid].time - now < 48 && msg.value== openEvents[_eventid].price_back)
-	        openEvents[_eventid].wallet = wallet_ - ((openEvents[_eventid].price_back*80)/100);
-	   
-	    else if(openEvents[_eventid].time - now > 48 && msg.value== openEvents[_eventid].price_centre)
-	        openEvents[_eventid].wallet = wallet_ - (openEvents[_eventid].price_back);
        
-        openEvents[_eventid].sold -= 1;
-        delete tickets[_seatno];
+        // delete tickets[_seatno];
 
 	}
 	
-	function cancelevent(uint _eventid) public goodTime(openEvents[_eventid].time)
+	function cancelEvent(uint _eventId) public eventExist(_eventId)
 	{
-	   delete openEvents[_eventid];
+       OpenEvent memory _event = openEvents[_eventId];
+       
+	   eventExists[_eventId] = 0;
+	   
+	   for(uint256 i=1;i<_event.total_seats;i++)
+	   {
+	       if(Record[_eventId][i] != address(0))
+	       {
+	           _event.wallet -= _event.prices[_event.booked[i][1] - 1];
+	       }
+	   }
 	}
 }
