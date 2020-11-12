b9dc6654fd1669044ed29a64307c62f3b492b0a0
Mridul Dey <mridul.dgp2015@gmail.com>
2020-11-12 15:11:13 +0530

    Add necessary events

--- contracts/BookingDappManager.sol
+++ contracts/BookingDappManager.sol
@@ -12,9 +12,13 @@ contract BookingDappManager
     
     address public owner;
     mapping(address => bool) public events;
-    uint256 eventId = 0;
+    uint256 public totalEvents = 0;
     
-    event NewEvent(uint256, address, address);
+    // address[] public allEvents;
+
+    event NewEvent(uint256, address indexed, address, string, string, uint256);
+    event BoughtTickets(address indexed _event, address indexed buyer, uint256[] seats, string name, string location, uint256 startTime);
+    event CancelTickets(address indexed _event, address indexed buyer, uint256[] seats, string name, string location, uint256 startTime);
     
     constructor()
     {
@@ -22,17 +26,18 @@ contract BookingDappManager
     }
     
     
-    function createEvent(string memory _name, string memory _location, uint256 _startTime, uint256 _seatTypes, uint256[] memory _seatsPerType, uint256[] memory _pricePerType) public
+    function createEvent(string memory _name, string memory _desc, string memory _location, uint256 _startTime, uint256 _seatTypes, uint256[] memory _seatsPerType, uint256[] memory _pricePerType) public
     {
         uint256 totalSeats = 0;
         for(uint256 i=0; i<_seatTypes; i++)
             totalSeats += _seatsPerType[i];
         
-        eventId++;
+        totalEvents++;
         
         EventManager _newEvent = new EventManager(
             msg.sender,
             _name,
+            _desc,
             _location,
             _startTime,
             _seatTypes,
@@ -42,8 +47,20 @@ contract BookingDappManager
         );
         
         events[address(_newEvent)] = true;
+        // allEvents.push(address(_newEvent));
         
-        emit NewEvent(eventId, address(_newEvent), msg.sender);
+        emit NewEvent(totalEvents, msg.sender, address(_newEvent), _name, _location, _startTime);
     }
     
+    function emitTickets(address buyer, uint256[] memory seats, string memory name, string memory location, uint256 startTime) external
+    {
+        require(events[msg.sender], "Event does not exist");
+        emit BoughtTickets(msg.sender, buyer, seats, name, location, startTime);
+    }
+    
+    function emitCancel(address buyer, uint256[] memory seats, string memory name, string memory location, uint256 startTime) external
+    {
+        require(events[msg.sender], "Event does not exist");
+        emit CancelTickets(msg.sender, buyer, seats, name, location, startTime);
+    }
 }
